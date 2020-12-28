69639fadeed3dc67ece6bac656a2ae9fbf499aff
cmguo <gcm@ustc.edu>
2020-12-28 19:44:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Word.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.lang.reflect.Method;
+
+@Entity(tableName = "WORD")
+public class Word {
+    @PrimaryKey(autoGenerate = true)
+    private  int id;
+
+    @ColumnInfo(name = "english_word")
+    private String word;
+
+    @ColumnInfo(name = "chinese_mearning")
+    private String chineseMeaning;
+
+    public Word(String word, String chineseMeaning) {
+        this.word = word;
+        this.chineseMeaning = chineseMeaning;
+        this.getClass().getName();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getChineseMeaning() {
+        return chineseMeaning;
+    }
+
+
+    // set
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setChineseMeaning(String chineseMeaning) {
+        this.chineseMeaning = chineseMeaning;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/WordDao.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao    //Database access object
+public interface WordDao {
+    @Insert
+    void  insertWords(Word... words);
+    @Update
+    int updateWords(Word... words);
+    @Delete
+    void deleteWords(Word...words);
+
+    @Query("DELETE FROM WORD")
+    void deleteWords();
+
+    @Query("SELECT * FROM WORD ORDER BY ID DESC")
+    List<Word> getAllWords();
+}
